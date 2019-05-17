a9f9e4e08b385ad6a6e4b368ce9666c1a51783ca
Derek Kraan <derek.kraan@gmail.com>
2019-05-17 09:13:47 +0200

    Bump version to 0.2.5

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Walkman.MixProject do
   def project do
     [
       app: :walkman,
-      version: "0.2.4",
+      version: "0.2.5",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
