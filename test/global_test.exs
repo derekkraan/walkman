d68efdc368d8d44e8013a4cefb966ffe53577ee2
Caio Ceccon <contact@caioceccon.com>
2019-10-15 19:02:16 +0200

    Add support for changing default tape mode

--- test/global_test.exs
+++ test/global_test.exs
@@ -15,4 +15,22 @@ defmodule GlobalTest do
       assert_receive(:done)
     end
   end
+
+  test "can set global as default" do
+    Application.put_env(:walkman, :global, true)
+
+    Walkman.use_tape "global test" do
+      test_pid = self()
+
+      spawn_link(fn ->
+        assert {:ok, "global echo"} = TestEchoWrapper.echo("global echo")
+        refute_receive("global echo", 10)
+        send(test_pid, :done)
+      end)
+
+      assert_receive(:done)
+    end
+
+    Application.put_env(:walkman, :global, false)
+  end
 end
